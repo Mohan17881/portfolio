a0d765f5fcb79b1af9bb8033ce054d44b4c64ee7
mohan178811 <mohankrishna17856>
2026-02-15 14:18:13 +0530

    adding files

---
+++ script.js
@@ -0,0 +1,10 @@
+var typed = new Typed('#element', {
+  strings: [
+    "Frontend Developer",
+    "Backend Developer",
+    "Building Modern Web Apps"
+  ],
+  typeSpeed: 60,
+  backSpeed: 40,
+  loop: true
+});
